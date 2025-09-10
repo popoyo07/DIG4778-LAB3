178da23542531977f5130b2e1d08414297732000
LilyW3865 <li733414@ucf.edu>
2025-09-10 15:03:58 -0400

    PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    void Start()
+    {
+        moveSpeed = 15f; 
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Move right");
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Move left");
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Move right");
+            transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("Move left");
+            transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+        }
+    }
+}
